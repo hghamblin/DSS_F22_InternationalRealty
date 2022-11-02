e05ae510c26fd7f39ea9f67628988c2be88be761
hghamblin <hghamblin@gmail.com>
2022-11-02 12:04:08 -0600

    remove files

--- bell_david/stuff.py
+++
@@ -1,3 +0,0 @@
-from selenium import webdriver
-driver = webdriver.Chrome("/Users/davidbell/Downloads/chromedriver 2")
-driver.close()
--- locallibrary/pages/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class URLForm(forms.Form):
-    url = forms.CharField(max_length=256)
-
-    def get_form_data(self):
-        print(self.cleaned_data)
